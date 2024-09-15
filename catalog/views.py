00d18df60c126ec3613d61fdbbcc8a7923138a07
vladimirbutyrskii <vlaver@mail.ru>
2024-09-15 23:35:31 +0300

    final commit

--- catalog/views.py
+++ catalog/views.py
@@ -10,8 +10,12 @@ from catalog.models import Product, Version
 class ProductListView(ListView):
     model = Product
 
-    # app_name/<model_name>_<action>  -  правило формирования названия шаблона
-    # catalog/product_list.html
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context_data = super().get_context_data(**kwargs)
+        for product in context_data['object_list']:
+            active_version = Version.objects.filter(product=product, active=True).first()
+            product.active_version = active_version
+        return context_data
 
 
 class ProductDetailView(DetailView):
@@ -43,7 +47,7 @@ class ProductUpdateView(UpdateView):
     def get_context_data(self, **kwargs):
         context_data = super().get_context_data(**kwargs)
         ProductFormset = inlineformset_factory(Product, Version, VersionForm, extra=1)
-        if self.request == 'POST':
+        if self.request.method == 'POST':
             context_data["formset"] = ProductFormset(self.request.POST, instance=self.object)
         else:
             context_data["formset"] = ProductFormset(instance=self.object)
